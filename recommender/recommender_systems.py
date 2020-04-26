712d7e55d58374714737493212b24b0936393b7c
bigname <tuanhiep.tran88@gmail.com>
2020-04-25 18:29:44 -0600

    re-architect project

--- recommender/recommender_systems.py
+++ recommender/recommender_systems.py
@@ -18,11 +18,10 @@ sc = spark.sparkContext
 import matplotlib.pyplot as plt
 import numpy as np
 
-"""#Extracting features from the MovieLens 100k dataset
-
+"""
+Extracting features from the MovieLens 100k dataset
 We will use the same MovieLens dataset [ml-100k.zip](http://files.grouplens.org/datasets/movielens/ml-100k.zip) that we 
 used in the Tutorial 3.
-
 Upload ml-100k.zip to Colab. Unzip it.
 """
 
@@ -33,20 +32,24 @@ Upload ml-100k.zip to Colab. Unzip it.
 rawData = sc.textFile("ml-100k/u.data")
 rawData.first()
 
-"""Recall that this dataset consisted of the user id, movie id, rating, timestamp fields separated by a tab ("\t") 
+"""
+Recall that this dataset consisted of the user id, movie id, rating, timestamp fields separated by a tab ("\t") 
 character. We don't need the time when the rating was made to train our model, so let's simply extract the first three
- fields:"""
+fields:
+"""
 
 rawRatings = rawData.map(lambda s: s.split("\t")[0:3])
 rawRatings.first()
 
-"""We will use Spark's MLlib library to train our model. Let's take a look at what methods are available for us to use
- and what input is required. First, import the ALS model from MLlib:"""
+"""
+We will use Spark's MLlib library to train our model. Let's take a look at what methods are available for us to use
+and what input is required. First, import the ALS model from MLlib:"""
 
 from pyspark.mllib.recommendation import ALS
 from pyspark.mllib.recommendation import Rating
 
-"""We need to provide the ALS model with an RDD that consists of Rating records. A Rating class, in turn, is just a 
+"""
+We need to provide the ALS model with an RDD that consists of Rating records. A Rating class, in turn, is just a 
 wrapper around user id, movie id (called product here), and the actual rating arguments.
 We'll create our rating dataset using the map method and transforming the array of IDs and ratings into a Rating object:
 """
@@ -54,16 +57,17 @@ We'll create our rating dataset using the map method and transforming the array 
 ratings = rawRatings.map(lambda l: Rating(int(l[0]), int(l[1]), float(l[2])))
 ratings.first()
 
-"""#Training a recommendation model on the MovieLens 100k dataset
+"""
+#Training a recommendation model on the MovieLens 100k dataset
 
 Besides Ratings, the other inputs required for our model
 are as follows:
-*  *rank*: This refers to the number of factors in our ALS model, that is, the number of hidden features in our low-rank
- approximation matrices. Generally, the greater the number of factors, the better, but this has a direct impact on memory
-  usage, both for computation and to store models for serving, particularly for large number of users or items. A rank 
-  in the range of 10 to 200 is usually reasonable.
+**rank*: This refers to the number of factors in our ALS model, that is, the number of hidden features in our low-rank
+approximation matrices. Generally, the greater the number of factors, the better, but this has a direct impact on memory
+usage, both for computation and to store models for serving, particularly for large number of users or items. A rank 
+in the range of 10 to 200 is usually reasonable.
 
-*  *iterations*: This refers to the number of iterations to run. ALS models will converge to a reasonably good solution 
+**iterations*: This refers to the number of iterations to run. ALS models will converge to a reasonably good solution 
 after relatively few iterations. Around 10 is often a good default.
 
 We'll use rank of 10, and 10 iterations to illustrate how to train our model:
@@ -71,25 +75,28 @@ We'll use rank of 10, and 10 iterations to illustrate how to train our model:
 
 model = ALS.train(ratings, 10, 10)
 
-"""This returns a MatrixFactorizationModel object, which contains the user and item factors in the form of an RDD of 
-(id, factor) pairs. These are called userFeatures and productFeatures, respectively. For example:"""
+"""
+This returns a MatrixFactorizationModel object, which contains the user and item factors in the form of an RDD of 
+(id, factor) pairs. These are called userFeatures and productFeatures, respectively. For example:
+"""
 
 model.userFeatures().collect()
 model.productFeatures().collect()
 
-"""#Using the recommendation model
+"""
+#Using the recommendation model
 
 Now that we have our trained model, we're ready to use it to make predictions.
 
 ##User recommendations
 
 In this case, we would like to generate recommended items for a given user. This usually takes the form of a top-K list,
- that is, the K items that our model predicts will have the highest probability of the user liking them. This is done by
-  computing the predicted score for each item and ranking the list based on this score.
+that is, the K items that our model predicts will have the highest probability of the user liking them. This is done by
+computing the predicted score for each item and ranking the list based on this score.
 
 In matrix factorization, because we are modeling the ratings matrix directly, the predicted score can be computed as the
- vector dot product between a user-factor vector and an item-factor vector. As MLlib's recommendation model is based on 
- matrix factorization, we can use the
+vector dot product between a user-factor vector and an item-factor vector. As MLlib's recommendation model is based on 
+matrix factorization, we can use the
 factor matrices computed by our model to compute predicted scores (or ratings) for a user.
 
 The *MatrixFactorizationModel* class has a convenient *predict* method that will compute a predicted score for a given 
@@ -110,7 +117,8 @@ num_items = 10
 topKRecs = model.recommendProducts(userId, num_items)
 print(topKRecs)
 
-"""##Inspecting the recommendations
+"""
+##Inspecting the recommendations
 
 We can give these recommendations a sense check by taking a quick look at the titles of the movies a user has rated and 
 the recommended movies. First, we need to load the movie data. We'll collect this data as a Map[Int, String] method 
@@ -121,35 +129,40 @@ movies = sc.textFile("ml-100k/u.item")
 titles = movies.map(lambda line: line.split("|")[0:2]).map(lambda array: (int(array[0]), array[1])).collectAsMap()
 titles[123]
 
-"""For our user 789, we can find out what movies they have rated, take the 10 movies with the highest rating, and then 
+"""
+For our user 789, we can find out what movies they have rated, take the 10 movies with the highest rating, and then 
 check the titles. We will do this now by first using the keyBy Spark function to create an RDD of key-value pairs from 
 our ratings RDD, where the key will be the user ID. We will then use the lookup function to return just the ratings for 
-this key (that is, that particular user ID) to the driver:"""
+this key (that is, that particular user ID) to the driver:
+"""
 
 moviesForUser = ratings.keyBy(lambda r: r.user).lookup(789)
 print(len(moviesForUser))
 
-"""We see that this user has rated 33 movies.
+"""
+We see that this user has rated 33 movies.
 
 Next, we will take the 10 movies with the highest ratings by sorting the
 moviesForUser collection using the rating field of the Rating object. We will then extract the movie title for the 
 relevant product ID attached to the Rating class from our mapping of movie titles and print out the top 10 titles with
- their ratings:
+their ratings:
 """
 
 moviesForUser.sort(key=lambda r: -r.rating)
 sc.parallelize(moviesForUser[0:10]).map(lambda rating: (titles[rating.product], rating.rating)).collect()
 
-"""Now, let's take a look at the top 10 recommendations for this user and see what the titles are using the same 
+"""
+Now, let's take a look at the top 10 recommendations for this user and see what the titles are using the same 
 approach as the one we used earlier (note that the recommendations are already sorted):"""
 
 sc.parallelize(topKRecs).map(lambda rating: (titles[rating.product], rating.rating)).collect()
 
-"""We leave it to you to decide whether these recommendations make sense.
+"""
+We leave it to you to decide whether these recommendations make sense.
 
-#Evaluating the performance of recommendation models
+*Evaluating the performance of recommendation models
 
-##Mean Square Error
+*Mean Square Error
 
 We will compute the MSE and RMSE metrics using RegressionMetrics.
 """
@@ -167,7 +180,8 @@ regressionMetrics = RegressionMetrics(predictedAndTrue)
 print("Mean Squared Error = ", regressionMetrics.meanSquaredError)
 print("Root Mean Squared Error ", regressionMetrics.rootMeanSquaredError)
 
-"""#Questions
+"""
+#Questions
 
 ##Question 1
 
